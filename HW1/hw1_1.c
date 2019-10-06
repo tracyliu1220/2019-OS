41994232d7ed703ed0aca0b60892a8334ed173e4
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2019-10-06 22:34:19 +0800

    HW1-1

--- HW1/hw1_1.c
+++ HW1/hw1_1.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <wait.h>
 
 #define MAX_LINE 80
 
 int main(void)
 {
-	char *arg[MAX_LINE/2+1]; /*command line arguments*/
+	char *arg[MAX_LINE/2+1] = {}; /*command line arguments*/
+    char *buf = malloc(MAX_LINE * sizeof(char));
 	int should_run = 1; /*flag to determine when to exit program*/
+    int opt_wait = 1; 
 	
 	while(should_run){
-		print("osh>");
+		printf("osh>");
 		fflush(stdout);
-		
+        opt_wait = 1;
+
 		/**
 		* your code!
 		* After reading user input, the step are:
@@ -19,6 +25,51 @@ int main(void)
 		* (2) the child process will invoke execvp()
 		* (3) if command included &, parent will not invoke wait()
 		*/
+        
+        int ret = read(0, buf, MAX_LINE);
+        buf[ret] = '\0';
+        
+        char *ptr = strtok(buf, " \n");
+        int arg_len = 0;
+        for (arg_len = 0; ptr != NULL; arg_len ++) {
+            if (!strncmp(ptr, "&", 1))
+                opt_wait = 0;
+            else
+                arg[arg_len] = ptr;
+            ptr = strtok(NULL, " \n");
+        }
+
+        // exit
+        if (!strncmp(arg[0], "exit", 4)) {
+            should_run = 0;
+            continue;
+        }
+
+
+        int pid = fork();
+
+        // parent
+        if (pid > 0) {
+            if (opt_wait) {
+                int status = -1;
+                wait(&status);
+            }
+            // clean
+            for (int i = 0; i < arg_len; i++)
+                arg[i] = NULL;
+            continue;
+        }
+
+        // exec
+        if (!strncmp(arg[0], "ls", 2))
+             execvp("ls", arg);
+        if (!strncmp(arg[0], "cat", 3))
+             execvp("cat", arg);
+        if (!strncmp(arg[0], "date", 4))
+             execvp("date", arg);
+        if (!strncmp(arg[0], "ps", 2))
+             execvp("ps", arg);
+        
 	}
 
 	return 0;
