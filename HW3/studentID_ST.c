ad8d045e7a017eb5a2d14bff9c18ad1144a491d4
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2019-11-11 13:26:14 +0800

    hw3 setup

---
+++ HW3/studentID_ST.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* function definitions */
+void sort(int list[]);
+
+int main (int argc, const char * argv[]) 
+{
+	/* Use STDIN (e.g. scanf, cin) to take the input */
+	/*
+		your code here
+	*/
+
+	/* Do the sorting */
+	sort(list);	
+
+	/* Use STDOUT (e.g. printf, cout) to output the sorted array */
+	/*
+		your code here
+	*/
+
+    return 0;
+}
+
+void sort(int list[])
+{
+	// Sorting algorithm can be brute‐force methods, e.g., bubble sort 
+	/*
+		your code here
+	*/
+}
+
